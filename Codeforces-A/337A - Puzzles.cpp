569a2f653d2070cc9bd96849b83c78d869e2fba3
Ekanto <umarekanto@gmail.com>
2021-11-13 23:28:44 +0600

    edited

--- Codeforces-A/337A - Puzzles.cpp
+++ Codeforces-A/337A - Puzzles.cpp
@@ -1,32 +1,29 @@
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int main(){
-    int i,n,m,MIN(0);
+    int n,m,MIN(0);
     int left,right;
     cin >> n >> m;
     int arr[1000];
     for(int i=0;i<m;++i){
         cin >> arr[i];
     }
-
+ 
     sort(arr,arr+m);
     left = arr[0];
     right  = arr[n-1];
     MIN = right- left; 
-
-
-    while(i<=(m-n)){
-        int res=0;
-        res = right - left; //5
+ 
+ 
+    for(int i=1;i<=m-n;++i){
+        int res = arr[n+i-1] - arr[i];
         if(res < MIN){
-            res = MIN;
-            left++;
-            right++;
+            MIN =res;
         }
-        i++;
+ 
     }
-
+ 
     cout << MIN;
 }
